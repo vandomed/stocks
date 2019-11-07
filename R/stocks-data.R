5f3ebc8d86222a73c5fecd62e7e04e5856a125fa
Dane Van Domelen <vandomed@gmail.com>
2019-11-07 00:36:42 -0500

    Added function

--- R/stocks-data.R
+++ R/stocks-data.R
@@ -1,12 +1,12 @@
 #' High-Yield ETFs from ETFdb.com and Inception Dates
-#' 
+#'
 #' @name highyield_etfs
 #' @docType data
 #' @source \url{http://etfdb.com/etfdb-category/high-yield-bonds/#etfs&sort_name=assets_under_management&sort_order=desc&page=2}
 NULL
 #'
 #' Largest 100 Market Cap ETFs (as of 3/2/18) and Inception Dates
-#' 
+#'
 #' @name largest_etfs
 #' @docType data
 #' @source \url{http://etfdb.com/compare/market-cap/}
@@ -24,8 +24,8 @@ NULL
 #' @docType data
 #' @source \url{https://investor.vanguard.com/mutual-funds/list?assetclass=bond#/mutual-funds/asset-class/month-end-returns}
 NULL
-#' 
-#' Vanguard Bond ETFs and Inception Dates 
+#'
+#' Vanguard Bond ETFs and Inception Dates
 #' @name vanguard_bond_etfs
 #' @docType data
 #' @source \url{https://investor.vanguard.com/etf/list?assetclass=bond#/etf/asset-class/month-end-returns}
@@ -66,103 +66,103 @@ NULL
 #' @docType data
 #' @source \url{https://investor.vanguard.com/etf/list?assetclass=bond#/etf/asset-class/month-end-returns}
 NULL
-#' 
+#'
 #' Vanguard International Mutual Funds and Inception Dates
 #' @name vanguard_international_funds
 #' @docType data
 #' @source \url{https://investor.vanguard.com/mutual-funds/list?assetclass=bond#/mutual-funds/asset-class/month-end-returns}
 NULL
-#' 
+#'
 #' Vanguard Large-cap Stock ETFs and Inception Dates
 #' @name vanguard_largecap_etfs
 #' @docType data
 #' @source \url{https://investor.vanguard.com/etf/list?assetclass=bond#/etf/asset-class/month-end-returns}
 NULL
-#' 
+#'
 #' Vanguard Large-cap Stock Mutual Funds and Inception Dates
 #' @name vanguard_largecap_funds
 #' @docType data
 #' @source \url{https://investor.vanguard.com/mutual-funds/list?assetclass=bond#/mutual-funds/asset-class/month-end-returns}
 NULL
-#' 
+#'
 #' Vanguard Mid-cap Stock ETFs and Inception Dates
 #' @name vanguard_midcap_etfs
 #' @docType data
 #' @source \url{https://investor.vanguard.com/etf/list?assetclass=bond#/etf/asset-class/month-end-returns}
 NULL
-#' 
+#'
 #' Vanguard Mid-cap Stock Mutual Funds and Inception Dates
 #' @name vanguard_midcap_funds
 #' @docType data
 #' @source \url{https://investor.vanguard.com/mutual-funds/list?assetclass=bond#/mutual-funds/asset-class/month-end-returns}
 NULL
-#' 
+#'
 #' Vanguard Sector & Specialty ETFs and Inception Dates
 #' @name vanguard_sector_etfs
 #' @docType data
 #' @source \url{https://investor.vanguard.com/etf/list?assetclass=bond#/etf/asset-class/month-end-returns}
 NULL
-#' 
+#'
 #' Vanguard Sector Mutual Funds and Inception Dates
 #' @name vanguard_sector_funds
 #' @docType data
 #' @source \url{https://investor.vanguard.com/mutual-funds/list?assetclass=bond#/mutual-funds/asset-class/month-end-returns}
 NULL
-#' 
+#'
 #' Vanguard Small-cap Stock ETFs and Inception Dates
 #' @name vanguard_smallcap_etfs
 #' @docType data
 #' @source \url{https://investor.vanguard.com/etf/list?assetclass=bond#/etf/asset-class/month-end-returns}
 NULL
-#' 
+#'
 #' Vanguard Small-cap Stock Mutual Funds and Inception Dates
 #' @name vanguard_smallcap_funds
 #' @docType data
 #' @source \url{https://investor.vanguard.com/mutual-funds/list?assetclass=bond#/mutual-funds/asset-class/month-end-returns}
 NULL
-#' 
+#'
 #' Vanguard Stock ETFs and Inception Dates
 #' @name vanguard_stock_etfs
 #' @docType data
 #' @source \url{https://investor.vanguard.com/etf/list?assetclass=bond#/etf/asset-class/month-end-returns}
 NULL
-#' 
+#'
 #' Vanguard Stock Mutual Funds and Inception Dates
 #' @name vanguard_stock_funds
 #' @docType data
 #' @source \url{https://investor.vanguard.com/mutual-funds/list?assetclass=bond#/mutual-funds/asset-class/month-end-returns}
 NULL
-#' 
+#'
 #' Vanguard Target Date Mutual Funds
 #' @name vanguard_targetdate_funds
 #' @docType data
 #' @source \url{https://investor.vanguard.com/etf/list?assetclass=bond#/etf/asset-class/month-end-returns}
 NULL
-#' 
+#'
 #' Vanguard Target Risk Mutual Funds and Inception Dates
 #' @name vanguard_targetrisk_funds
 #' @docType data
 #' @source \url{https://investor.vanguard.com/etf/list?assetclass=bond#/etf/asset-class/month-end-returns}
 NULL
-#' 
+#'
 #' Vanguard Tax-exempt Bond Mutual Funds and Inception Dates
 #' @name vanguard_taxexempt_bond_funds
 #' @docType data
 #' @source \url{https://investor.vanguard.com/etf/list?assetclass=bond#/etf/asset-class/month-end-returns}
 NULL
-#' 
+#'
 #' Vanguard Traditional Mutual Funds and Inception Dates
 #' @name vanguard_traditional_funds
 #' @docType data
 #' @source \url{https://investor.vanguard.com/etf/list?assetclass=bond#/etf/asset-class/month-end-returns}
 NULL
-#' 
+#'
 #' Vanguard Treasury/Agency Bond ETFs and Inception Dates
 #' @name vanguard_treasury_etfs
 #' @docType data
 #' @source \url{https://investor.vanguard.com/etf/list?assetclass=bond#/etf/asset-class/month-end-returns}
 NULL
-#' 
+#'
 #' Vanguard Treasury/Agency Bond Mutual Funds and Inception Dates
 #' @name vanguard_treasury_funds
 #' @docType data
@@ -174,4 +174,11 @@ NULL
 #' @aliases metric.info
 #' @docType data
 #' @source Original
+NULL
+#'
+#' Lookup Table for Wikipedia S&P 500 Pages
+#' @name sp500_dates
+#' @aliases sp500.dates
+#' @docType data
+#' @source Wikipedia
 NULL
